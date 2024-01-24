0113455973982443484f167652bb0b94ad27b89f
key <30455795+keyhsw@users.noreply.github.com>
2024-01-24 12:59:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from openxlab.model import download
 
 base_path = './internlm2-chat-7b'
 
-download(model_repo='OpenLMLab/internlm2-chat-7b',output=base_path)
+# download(model_repo='OpenLMLab/internlm2-chat-7b',output=base_path)
+
+#模型下载
+from modelscope import snapshot_download
+model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm2-chat-7b',local_dir=base_path)
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
